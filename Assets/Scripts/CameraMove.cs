8fef5f7e2c3630da2232083f36eab982cc71b20e
Andrey <an.i.nesterov@gmail.com>
2023-06-08 15:42:21 +0300

    Refactoring

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,21 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         var horInput = Input.GetAxis("Horizontal");
         var verInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right* horInput/10);
-        transform.Translate(Vector3.forward * verInput/10);
+
+        transform.Translate(Vector3.right * horInput / 10);
+        transform.Translate(Vector3.forward * verInput / 10);
     }
 }
